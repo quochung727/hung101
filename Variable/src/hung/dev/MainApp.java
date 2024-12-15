317babaa580f30344de2dd63619912ad37dc0649
quochung727 <hungdeptrai2k95@gmail.com>
2024-12-15 16:13:06 +0700

    Add files via upload

---
+++ Variable/src/hung/dev/MainApp.java
@@ -0,0 +1,30 @@
+package hung.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 9;
+        Person p = new Person();
+        p.name="Person 1";
+        
+        p.nameSchool = "HaNoi UNI";
+        
+        Person p2 = new Person();
+        p2.name = "Person 2";
+        
+        p2.nameSchool = "DaNang UNI";
+        
+        System.out.println(p.name);
+        System.out.println(p.nameSchool);
+        
+        System.out.println(p2.name);
+        System.out.println(p2.nameSchool);
+        
+        System.out.println(Person.nameSchool);
+        
+        final double PI = 3.14;
+        
+	}
+
+}
