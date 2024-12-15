6adcf6d398b3669c9f24125954e565cb8a017ee7
quochung727 <hungdeptrai2k95@gmail.com>
2024-12-15 16:21:48 +0700

    Add files via upload

---
+++ LoopWhile/src/Basic/dev/MainApp2.java
@@ -0,0 +1,17 @@
+package Basic.dev;
+
+public class MainApp2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i = 0;
+        int tong = 0;
+        while (i <= 100 ) {
+        	i++;
+        	tong += i; //tong = tong +i
+        	System.out.println(i);
+        }
+        System.out.println("tong " + tong);
+    }
+
+}
